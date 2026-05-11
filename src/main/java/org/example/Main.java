7bc95ee07a6077608d166701f4c4f0a38ca2f4ae
RazvanPorumbescu <prbrazv@gmail.com>
2026-05-11 10:36:14 +0300

    print 100 numbers

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,5 @@
 package org.example;
-
+import java.util.ArrayList
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -12,6 +12,14 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             IO.println("i = " + i);
+        }
+    }
+    static void getNumbers(){
+        ArrayList<Integer> numbers=new ArrayList<>();
+        for(int i=1;i<=100;i++)
+        {
+            numbers.add(i);
+            System.out.println(i);
         }
     }
 }
